cc18cba374e3e7abfd48787fb87a64723b2e3ac4
Garcia_JeffQuinSyler <jeff@gmail.com>
2024-02-26 13:27:39 +0800

    Add 2 Features and CSS Design

---
+++ script.js
@@ -0,0 +1,4 @@
+function updateDateTime() { 
+const now = new Date(); 
+ const currentDateTime = now.toLocaleString(); document.querySelector('#datetime').textContent = currentDateTime; } 
+ setInterval(updateDateTime, 1000);
